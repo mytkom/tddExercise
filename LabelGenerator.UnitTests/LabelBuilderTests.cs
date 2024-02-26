869865e7ee2f26f21f00fbf7ebe619fe77bd27d9
mytkom <marek.mytkowski.mm@gmail.com>
2024-02-26 09:51:00 +0000

    Add ChangePosition method

--- LabelGenerator.UnitTests/LabelBuilderTests.cs
+++ LabelGenerator.UnitTests/LabelBuilderTests.cs
@@ -3,20 +3,44 @@ namespace LabelGenerator.UnitTests;
 public class LabelBuilderTests
 {
     [Theory]
-    [InlineData("")]
-    [InlineData("LabelText")]
-    [InlineData("LabelText Second")]
-    public void GenerateZplWithProvidedLabel(string labelText)
+    [InlineData("LabelText", $"^XA\n^FDLabelText\n^XZ\n")]
+    [InlineData("LabelText Second", $"^XA\n^FDLabelText Second\n^XZ\n")]
+    public void GeneratesCorrectZplWithProvidedLabel(string labelText, string expectedCode)
     {
       // Arrange
       LabelBuilder builder = new LabelBuilder(); 
-      string expectedResult = $"^XA\n^FD{labelText}\n^XZ\n";
       
       // Act
       builder.AddLabel(labelText);      
       string result = builder.GetResultCode();
 
       // Assert
-      Assert.Equal(expectedResult, result);
+      Assert.Equal(expectedCode, result);
+    }
+
+    [Fact]
+    public void RaisesExceptionWhenLabelTextIsEmpty()
+    {
+      // Arrange
+      LabelBuilder builder = new LabelBuilder(); 
+      
+      // Assert
+      Assert.Throws<ArgumentNullException>(() => builder.AddLabel(""));
+    }
+
+    [Theory]
+    [InlineData(10, 10, "^XA\n^FO10, 10\n^XZ\n")]
+    [InlineData(200, 600, "^XA\n^FO200, 600\n^XZ\n")]
+    public void GeneratesCorrectZplWithPositionChange(int x, int y, string expectedCode)
+    {
+      // Arrange
+      LabelBuilder builder = new LabelBuilder(); 
+      
+      // Act
+      builder.ChangePosition((x, y));      
+      string result = builder.GetResultCode();
+
+      // Assert
+      Assert.Equal(expectedCode, result);
     }
 }
--- LabelGenerator/LabelBuilder.cs
+++ LabelGenerator/LabelBuilder.cs
@@ -2,21 +2,47 @@ namespace LabelGenerator;
 
 using System.Text;
 
+enum ZplCodes {
+  AddLabel,
+  ChangePosition,
+
+};
+
 public class LabelBuilder
 {
-  StringBuilder ZplResultCodeBuilder;
+  private StringBuilder _zplResultCodeBuilder;
+  private (int x, int y) _positionLimits = (3200, 3200);
+  private Dictionary<ZplCodes, Func<string, string>> _operations = new Dictionary<ZplCodes, Func<string, string>>() {
+    {ZplCodes.AddLabel, a => $"^FD{a}"},
+    {ZplCodes.ChangePosition, a => $"^FO{a}"}
+  };
 
+  public string GetResultCode() {
+    return _zplResultCodeBuilder.ToString() + "^XZ\n";
+  }
   public LabelBuilder()
   {
-    ZplResultCodeBuilder = new StringBuilder("^XA\n");
+    _zplResultCodeBuilder = new StringBuilder("^XA\n");
   }
 
   public void AddLabel(string labelText)
   {
-    ZplResultCodeBuilder.Append($"^FD{labelText}\n");
+    if(labelText.Length == 0)
+    {
+      throw new ArgumentNullException();
+    }
+
+    AddLine(ZplCodes.AddLabel, labelText);
   }
 
-  public string GetResultCode() {
-    return ZplResultCodeBuilder.ToString() + "^XZ\n";
+  public void ChangePosition((int x, int y) position)
+  {
+
+    AddLine(ZplCodes.ChangePosition, $"{position.x}, {position.y}");
+  }
+
+  private void AddLine(ZplCodes code, string argument)
+  {
+    _zplResultCodeBuilder.Append($"{_operations[code](argument)}\n");
   }
 }
